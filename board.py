e605f0fae4248bd49f94d8ef356ab409d58614a4
morgan-a-bauer <131829467+morgan-a-bauer@users.noreply.github.com>
2023-06-15 13:05:47 -0400

    Started writing Board class

---
+++ board.py
@@ -0,0 +1,14 @@
+class Board:
+    def __init__(self):
+        self.state = [[0 for i in range(8)] for j in range(8)]
+
+    def __str__(self):
+        printed_board = []
+        for row in self.state:
+            row = [str(space) for space in row]
+            printed_board.append(''.join(row))
+        return '\n'.join(printed_board)
+
+if __name__ == "__main__":
+    my_board = Board()
+    print(my_board)
