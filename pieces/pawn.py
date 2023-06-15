3617b9810b319c82dee7f44d1111a8b1038778a9
morgan-a-bauer <131829467+morgan-a-bauer@users.noreply.github.com>
2023-06-15 13:31:58 -0400

    Started building pawn class

---
+++ pieces/pawn.py
@@ -0,0 +1,25 @@
+class Pawn:
+    def __init__(self, space: tuple) -> None:
+        self._chr = 'p'
+        self._color = ''
+        self._space = space
+
+
+    def __str__(self) -> str:
+        return self._chr
+
+
+    def get_color(self) -> str:
+        return self._color
+    
+
+    def set_color(self, color: str) -> None:
+        self._color = color
+    
+
+    def get_space(self) -> tuple:
+        return self._space
+    
+
+    def set_space(self, new_space: tuple) -> None:
+        self._space = new_space
