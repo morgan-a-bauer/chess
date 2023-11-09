fd301efc9693520912f060651f93803c98f988e5
Morgan Bauer <morgan.a.bauer@gmail.com>
2023-11-09 11:50:03 -0500

    Test Comment

--- pieces/bishop.py
+++ pieces/bishop.py
@@ -5,7 +5,7 @@ Defines an inherited class Bishop with methods specific to the mechanics of
 bishop movement
 """
 from pieces.base_piece import BasePiece
-
+"hello there"
 class Bishop(BasePiece):
     def __init__(self, space: tuple, color: str) -> None:
         BasePiece.__init__(self, space, color, 'b')
