247bb97c7c05e8b06e755d29294e29e02cc74d66
jacksoncbutler <jackson@butler.net>
2024-01-19 12:56:31 -0500

    started work on pgn to json conversion

---
+++ pgn_to_json.py
@@ -0,0 +1,32 @@
+"""
+    Jackson Butler
+    Converts a folder of pgns to a single large json object
+    This will be cleaned up, just my prototyping process :)
+"""
+
+import json
+import os
+import re
+
+pgnLocation = os.path.abspath("lichess_database")
+# pgnList = sorted([file for file in os.listdir(pgnLocation) if os.path.join(pgnLocation, file).endswith(".pgn")], key=lambda x:x[14:-4], reverse=True)
+# print(pgnList)
+
+pgnJsonObject = {}
+pgnList = ('lichess_elite_2013-09.pgn',)
+
+for file in pgnList:
+    with open(os.path.join(pgnLocation, file), 'r') as pgn:
+        pgnContent = pgn.readlines()
+        print(pgnContent[:50])
+        for pgnLine in pgnContent[:20]:
+            result = re.sub("[\[\]]", '', pgnLine).strip()
+            print(result)
+            result = re.split(r'\s(?=(?:[^"]*"[^"]*")*[^"]*$)', result, maxsplit=1)
+            result[1] = result[1].strip('"')
+            print(result)
+            
+            # Create clause for empty lines and then actual game moves
+        
+        
+        
